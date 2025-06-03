d648e8b04a3d7bd17ad51bd4feb4a3fd9f0f68d7
Rainerwald <Rainer.Hentschel@mailbox.org>
2025-06-03 09:20:44 +0200

    Update config.mjs
    Name der Surveys nun doch als Abkürzung gemäß online doku in der Listenauswahl

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -68,8 +68,8 @@ export default defineConfig({
           text: 'ICP Forests',
           link: '/dauerbeobachtung/icp-forests/index',
           items: [
-            { text: 'Meteo', link: '/dauerbeobachtung/icp-forests/meteo'},
-            { text: 'Visual Assessment of Crown Condition', link: '/dauerbeobachtung/icp-forests/crown_condition' },
+            { text: 'mm', link: '/dauerbeobachtung/icp-forests/meteo'},
+            { text: 'cc', link: '/dauerbeobachtung/icp-forests/crown_condition' },
           ]
         },
         {
