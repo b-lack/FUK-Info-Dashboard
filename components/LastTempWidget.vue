c8aae4540446fe7d047cd5224e1a2a819d175b89
linus lissel <l.lissel@gmx.de>
2025-08-06 11:44:17 +0200

    continue debugging lastTempWidget
    store query results, display them on website, not reliable running yet

--- components/LastTempWidget.vue
+++ components/LastTempWidget.vue
@@ -14,29 +14,39 @@
     let loading = ref(false);
     let lastTemp = ref(null);
 
-    watch(onMounted( async () => {
-        console.log('Mounted LastTempWidget');
-        loading.value = true;
-        supabase
-            .schema('icp_download')
-            .from('mm_mem')
-            .select('date_observation, daily_max, daily_min, daily_mean, code_variable, instrument_seq_nr') // use * to get all contents
-            .eq('code_plot', code_plot.value)
-            .eq('code_variable', code_variable.value)
-            .eq('instrument_seq_nr', instrument_seq_nr.value)
-            .order('date_observation', { ascending: false })
-            .limit(1).single
+    onMounted( async () => {
+        // console.log('Mounted LastTempWidget');
+        loading.value = true
+        // console.log(loading.value)
+        try{
+            lastTemp = await supabase
+                .schema('icp_download')
+                .from('mm_mem')
+                // .select('date_observation, daily_max, daily_min, daily_mean, code_variable, instrument_seq_nr')
+                .select('*')
+                // use * to get all contents
+                .eq('code_plot', code_plot.value)
+                .eq('code_variable', code_variable.value)
+                .eq('instrument_seq_nr', instrument_seq_nr.value)
+                .order('date_observation', { ascending: false })
+                .limit(1).single()
+            console.log('try statement runs through till here')
 
-            try {
-                console.log('Fetching last temperature data for plot:', data)
-                return lastTemp.value = data;
-            }
-            catch (error) {}
+            lastTemp = lastTemp.data
+            console.log(lastTemp)
+            // return lastTemp.value = data;
+            console.log(lastTemp.date_observation)
+            return lastTemp
+        }
+        catch (error) {
+            console.log('this is an error')
+        }
 
-            finally {
-                loading.value = false;
-            };
-    }));
+        finally {
+            loading.value = false;
+            console.log(loading.value)
+        };
+    });
 
     // definition der anderen plots für später
     const plots = {
@@ -55,13 +65,13 @@
 </script>
 
 <template>
-    <p>
-    {{ lastTemp.dateObservation }} -
-    {{ lastTemp.daily_mean }} -
-    {{ lastTemp.daily_max}} -
-    {{ lastTemp.daily_min }}
+    <div>
+    observation date: {{ lastTemp.date_observation }} |
+    mean temp: {{ lastTemp.daily_mean }}C |
+    max temp: {{ lastTemp.daily_max }}C |
+    min temp: {{ lastTemp.daily_min }}C
 
     <!-- include a loader/ loading screen/bar-->
-    </p>
+    </div>
 
 </template>
